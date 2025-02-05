7bd0fcdfe8ea9369c3c6f2fe5a915da8c62399d3
SLG-DEVX <prudenceelise2009@gmail.com>
2025-02-05 03:47:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ main();
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
     res.send("hey, bot started ✔️");
