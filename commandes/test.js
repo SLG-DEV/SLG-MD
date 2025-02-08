471ba7511dcc615b03e72b3c8bf591c457b38ba1
SLG-DEVX <prudenceelise2009@gmail.com>
2025-02-08 17:00:10 +0100

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -11,7 +11,7 @@ async {pseudo, ms_org, com_options} => {
 
 const mess = `salut ${pseudo} je suis SLG-MD\nun bot développé par S L² G\n`
 console.log("le bot est en ligne");
-const url =  ""
-slg.sendMessage(ms_org,{image:{url: ''}, caption: mess})
+const rl =  ""
+slg.sendMessage(ms_org,{image:{url: 'rl'}, caption: mess})
 
 }
