50cd71d68211459378a9ab590c980e45a3926951
SLG-DEVX <prudenceelise2009@gmail.com>
2025-02-10 09:29:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ dotenv.config();
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "SLG-MD~xteDRWA5",
 OWNER: process.env.NUMERO_OWNER || "237620066719", 
-STATUS: process.env.LECTURE_AUTO_STATUS || 'oui', 
+STATUS: process.env.LECTURE_AUTO_STATUS || "oui", 
  PREFIX: process.env.PREFIX || ".",
    MODE: process.env.MODE || "public",
 PRESENCE: process.env.PRESENCE || "ecrit", // online , ecrit, audio ou mettez non si vous ne désirez aucune des options
