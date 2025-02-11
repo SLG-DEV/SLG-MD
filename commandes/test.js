5ea3602e78def925ad45c23d46175929bc685d20
SLG-DEVX <prudenceelise2009@gmail.com>
2025-02-11 07:12:22 +0100

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -1,18 +1,15 @@
-// the firts commandes created 
+// the first command created 
 const slgcomd = require("../lib/slgcomd");
 
 slgcomd({ 
-
   nomCom: "test",
   classe: "utiles",
   react: "🔋"
-
-} 
+}, // Ajoutez une virgule ici
 async (ms_org, slg, com_options) => {
-const {pseudo} = com_options
-const mess = `salut ${pseudo} je suis SLG-MD\nun bot développé par S L² G\n`
-console.log("le bot est en ligne");
-const rl =  "https://files.catbox.moe/uih7xz.jpg"
-slg.sendMessage(ms_org,{image:{url: rl}, caption: mess})
-
+    const {pseudo} = com_options;
+    const mess = `salut ${pseudo} je suis SLG-MD\nun bot développé par S L² G\n`;
+    console.log("le bot est en ligne");
+    const rl = "https://files.catbox.moe/uih7xz.jpg";
+    slg.sendMessage(ms_org, {image: {url: rl}, caption: mess});
 });
