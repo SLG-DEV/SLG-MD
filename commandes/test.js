a263b797c0560cbd6cf3fc1cb674774fc00ff98f
SLG-DEVX <prudenceelise2009@gmail.com>
2025-02-16 18:32:13 +0100

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -1,5 +1,7 @@
 // the first command created 
 const { slgcomd } = require("../lib/slgcomd");
+const os = require("os");
+const {ZONE_DE_TEMPS} = require("../config");
 
 slgcomd({ 
   nomCom: "test",
@@ -32,3 +34,24 @@ const ping = ng - pi
 slg.sendMessage(ms_org,{text:`> pong ${ping} ms`});
 });
 
+
+const runtime = (seconds) => {
+  const J = Math.floor(seconds / (3600 * 24));
+  const H = Math.floor((seconds % (3600 * 24)) / 3600);
+  const M = Math.floor((seconds % 3600) / 60);
+  const S = Math.floor(seconds % 60);
+  return `${J > 0 ? J + " J " : ""}${H > 0 ? H + " H " : ""}${M > 0 ? M + " M " : ""}${S > 0 ? S + " S" : ""}`;
+};
+
+slgcomd({
+nomCom: "alive",
+desc: "temps de fonctionnement",
+react "🍷"
+}, 
+async ( ms_org, slg ) => 
+{ 
+const [date, time] = new Date()
+      .toLocaleString("en-IN", { timeZone: ZONE_DE_TEMPS })
+      .split(",");
+
+const mes = `*NOUS SOMMES LE ${date} AVEC UN UPTIME DE ${runtime().trim()} A ${time}`
