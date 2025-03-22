64544392c1dccc1e3e68650dea9b1f8a06576a9a
SLG-DEVX <prudenceelise2009@gmail.com>
2025-03-22 02:50:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "SLG-MD~G9Fnsffz",
 OWNER: process.env.NUMERO_OWNER || "237620066719", 
 STATUS: process.env.LECTURE_AUTO_STATUS || "oui", 
- PREFIX: process.env.PREFIX || ".",
+ PREFIX: process.env.PREFIX || "null",
    MODE: process.env.MODE || "public",
 PRESENCE: process.env.PRESENCE || "audio", // online , ecrit, audio ou mettez non si vous ne désirez aucune des options
 ZONE_DE_TEMPS: process.env.ZONE_DE_TEMPS || "Africa/Lagos",
