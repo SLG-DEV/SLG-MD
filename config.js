2b934cb3e13bcea7b6f1b82a53e4ab08a69888b2
SLG-DEVX <prudenceelise2009@gmail.com>
2025-03-23 10:47:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "SLG-MD~Tu8Usp2Y",
+  SESSION_ID: process.env.SESSION_ID || "SLG_MD=Hzw3BZ7S#PMwLmlAIWuJh9fzt-Unj_X2FS-AYcKQj4Q1v0wAixHk",
 OWNER: process.env.NUMERO_OWNER || "237620066719", 
 STATUS: process.env.LECTURE_AUTO_STATUS || "oui", 
  PREFIX: process.env.PREFIX || "null",
