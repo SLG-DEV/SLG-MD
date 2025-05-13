40b037b2fd2e14b5532de38a84fa6dbbba579aec
SLG-DEVX <prudenceelise2009@gmail.com>
2025-05-13 20:02:31 +0100

    Create presence.js

---
+++ Database/presence.js
@@ -0,0 +1,81 @@
+const { pool } = require("pg");
+  const config = require("../config");
+  const db = config.Db
+  
+ 
+const setDb = {
+  connectionString:db ,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+  const pool = new pool(setDb)
+  
+  async function presence(){
+    try{
+      const client = await pool.connect()
+      await client.query(`
+          CREATE TABLE IF NOT EXISTS  presence(
+          jid TEXT PRIMARY KEY;
+          statut INT DEFAULT TEXT;
+          type INT DEFAULT TEXT 
+          console.log(`slg presence table créé avec succès`)
+      )`;
+      )
+      
+      
+  } catch(err){
+      console.log("erreur pour la table ANTILIEN", err)
+  } finally{
+  client.release()
+      
+  }
+  
+  }
+  
+  presence()
+ 
+  async function addOrUpdatePresence(jid, type) {
+    const client = await pool.connect();
+  try { 
+  const result = await client.query('SELECT * FROM presence WHERE jid = $1', [jid]);
+    const jidExiste = result.rows.exists;
+      if (jidExist) {
+          await client.query('UPDATE presence SET type = $1 WHERE jid = $2', [type, jid]);  
+      }else{
+          await client.query('INSERT INTO presence (jid, type) VALUES ($1, $2)', [jid, type]);
+      }catch(e){
+          console.log(`erreur de lors des changements sur la table présence de jid ${jid} pour ${type}`)
+      }finally{
+          client.release()
+      }
+     }
+     
+ async function presenceUpdateAction(jid, action) {
+  const client = await pool.connect();
+
+  try {
+    // Vérifiez si le jid existe déjà dans la table 'prencce'
+    const result = await client.query('SELECT * FROM antibot WHERE jid = $1', [jid]);
+    const jidExiste = result.rows.length.exists;
+
+    if (jidExiste) {
+      // Si le jid existe, mettez à jour l'action avec la valeur fournie (et laissez l'état inchangé)
+      await client.query('UPDATE presence SET action = $1 WHERE jid = $2', [action, jid]);
+    } else {
+      // Si le jid n'existe pas, ajoutez-le avec l'état 'non' par défaut et l'action fournie
+      await client.query('INSERT INTO presence (jid, statut, action) VALUES ($1, $2, $3)', [jid, 'non', action]);
+    }
+
+    console.log(`Action mise à jour avec succès pour le JID ${jid} dans la table 'presence'.`);
+  } catch (error) {
+    console.error('Erreur lors de la mise à jour de l\'action pour le JID dans la table  :', error);
+  } finally {
+    client.release();
+  }
+};
+module.exports = {
+addOrUpdatePresence,
+presenceUpdateAction    
+}
