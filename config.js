b2370eab1eaa4554e051843ba078a11462126737
SLG-DEVX <prudenceelise2009@gmail.com>
2025-05-14 14:07:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,5 @@ OWNER: process.env.NUMERO_OWNER || "237620066719",
 STATUS: process.env.LECTURE_AUTO_STATUS || "oui", 
  PREFIX: process.env.PREFIX || "null",
    MODE: process.env.MODE || "prive",
-PRESENCE: process.env.PRESENCE || "audio", // online , ecrit, audio ou mettez non si vous ne désirez aucune des options
 ZONE_DE_TEMPS: process.env.ZONE_DE_TEMPS || "Africa/Lagos",
 };
