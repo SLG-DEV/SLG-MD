1ae86ae58db27999591d836a512e0fe6ea3438dd
SLG-DEV <marco56917@xffc.undeadbank.com>
2025-05-17 22:59:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "SLG-MD~sKPPSVLp",
+  SESSION_ID: process.env.SESSION_ID || "SLG-MD~uNqU11HT",
 OWNER: process.env.NUMERO_OWNER || "237620066719", 
 STATUS: process.env.LECTURE_AUTO_STATUS || "oui", 
  PREFIX: process.env.PREFIX || "null",
