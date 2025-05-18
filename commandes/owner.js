2402babb508ff2247899e945dd2ec19f99b06378
SLG-DEV <marco56917@xffc.undeadbank.com>
2025-05-18 05:26:52 +0100

    Update owner.js

--- commandes/owner.js
+++ commandes/owner.js
@@ -1,4 +1,7 @@
-const {addOrUpdatePresence,
-presenceUpdateActionJid} = require("../Database/presence");
+const {pAddOrUpdateJid,
+pUpdateActionJid} = require("../Database/presence");
 const{ addSudoNumber,  delSudoNumber,  getAllSudoNumbers } = require("../Database/sudo");
 const { slgcomd } = require("../lib/slgcomd");
+
+
+
