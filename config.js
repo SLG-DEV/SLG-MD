569c1517969f99e172c62ce631b9350111e3393a
SLG-DEV <marco56917@xffc.undeadbank.com>
2025-05-30 19:21:52 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "SLG-MD~qJZai67Z",
 OWNER: process.env.NUMERO_OWNER || "237620066719", 
 STATUS: process.env.LECTURE_AUTO_STATUS || "oui", 
- PREFIX: process.env.PREFIX || "null",
+ PREFIX: process.env.PREFIX || ".",
    MODE: process.env.MODE || "prive",
 ZONE_DE_TEMPS: process.env.ZONE_DE_TEMPS || "Africa/Lagos",
    Db: process.env.Db || "postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres",
