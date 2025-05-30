b16839abf1aaae5f24d0fe079ce98451ce32488b
SLG-DEV <marco56917@xffc.undeadbank.com>
2025-05-30 18:19:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const {
     makeInMemoryStore, 
     fetchLatestBaileysVersion, 
     DisconnectReason 
-} = require("baileys"); // Fin de configuration
+} = require("@whiskeysockets/baileys"); // Fin de configuration
 
 const credsPath = path.join(__dirname, 'auth', 'creds.json'); // Début du chemin d'auth
 async function slgAuth() { // Début de slgAuth
