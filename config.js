a61dda5e3f9b17fbb4d50cb63ff0204ad7c71092
SLG-DEV <marco56917@xffc.undeadbank.com>
2025-06-01 20:29:15 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ STATUS: process.env.LECTURE_AUTO_STATUS || "oui",
  PREFIX: process.env.PREFIX || ".",
    MODE: process.env.MODE || "public",
 ZONE_DE_TEMPS: process.env.ZONE_DE_TEMPS || "Africa/Lagos",
-   Db: process.env.Db || "postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres",
+   Db: process.env.Db || "postgresql://postgres.wallfwopzizhycaaggug:Slg_wa_bot1@aws-0-eu-central-1.pooler.supabase.com:5432/postgres",
 };
